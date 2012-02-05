1cefe4e9cf926dfa756f786bd89d4030fb6e22c3
Chris Dobell <chris-dobell@hotmail.com>
2012-02-04 18:01:32 -0700

    Initial many commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuitionmatch::Application.config.secret_token = 'ff2693cf48bdd34312f46560abd8185621f412131681030937cb8a9aceaa5c7d685a639d3bce4f5e923438d0934ad1151df6ad00ec2f0835f4e6d96801b27676'
